b377020779a2a17952155918253510e230bf38b3
alweb29 <101860553+alweb29@users.noreply.github.com>
2022-04-30 13:20:40 +0200

    first setup

--- finalproject/finalprojectsite/venv/Scripts/activate.bat
+++ finalproject/finalprojectsite/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User_PC\OneDrive\Dokumenty\GitHub\po-laboratorium-2122\FinalProject\venv
+set VIRTUAL_ENV=C:\Users\User_PC\OneDrive\Dokumenty\GitHub\po-laboratorium-2122\finalproject\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
