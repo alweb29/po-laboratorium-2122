1572daddd96db956b91f9086398c07afe3268f9e
alweb29 <101860553+alweb29@users.noreply.github.com>
2022-05-01 09:56:51 +0200

    models

--- finalproject/finalprojectsite/authors/admin.py
+++ finalproject/finalprojectsite/authors/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author
+from .models import Book
+from .models import Award
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Award)
--- finalproject/finalprojectsite/authors/models.py
+++ finalproject/finalprojectsite/authors/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    authorName_text = models.CharField(max_length=200)
+    birth_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.authorName_text
+
+class Book (models.Model):
+    author = models.ForeignKey(Author, on_delete= models.CASCADE)
+    bookTitle_text = models.CharField(max_length=200)
+    bookRate = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.bookTitle_text
+
+class Award (models.Model):
+    awardName_text = models.CharField(max_length=100)
+    awardedAuthors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.awardName_text
