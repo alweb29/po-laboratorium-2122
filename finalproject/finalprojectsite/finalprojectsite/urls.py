d52ceced589fe9bc19c37dab199e6fd7cd670a0b
alweb29 <101860553+alweb29@users.noreply.github.com>
2022-05-01 08:44:50 +0200

    authors list

--- finalproject/finalprojectsite/finalprojectsite/urls.py
+++ finalproject/finalprojectsite/finalprojectsite/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('authors/', include('authors.urls')),
     path('admin/', admin.site.urls),
-    path('authors/', include('authors.urls'))
 ]
